9500adb6fe71f37cff29b48ddbcbf400694d8428
Gugu1127 <80884134+Gugu1127@users.noreply.github.com>
2023-02-11 00:05:10 +0800

    Add files via upload

---
+++ cls_type/cls.py
@@ -0,0 +1,249 @@
+{
+  "type": "bubble",
+  "direction": "ltr",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "borderWidth": "5px",
+    "backgroundColor": "#101010C9",
+    "borderColor": "#FFFFFFFF",
+    "cornerRadius": "15px",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "lg",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "vertical",
+            "borderWidth": "3px",
+            "contents": [
+              {
+                "type": "text",
+                "text": "自然科學與人工智慧導論",
+                "weight": "bold",
+                "color": "#FFFFFFFF",
+                "align": "center",
+                "style": "italic",
+                "contents": [],
+                "adjustMode": "shrink-to-fit",
+                "size": "lg"
+              }
+            ]
+          },
+          {
+            "type": "separator",
+            "margin": "xxl",
+            "color": "#00000000"
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "課程代碼",
+                "color": "#BFBFBFFF",
+                "align": "start",
+                "margin": "xs",
+                "position": "relative",
+                "contents": [],
+                "adjustMode": "shrink-to-fit"
+              },
+              {
+                "type": "text",
+                "text": "AC000A",
+                "color": "#BFBFBFFF",
+                "align": "start",
+                "contents": [],
+                "adjustMode": "shrink-to-fit"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "必修",
+                "color": "#BFBFBFFF",
+                "align": "start",
+                "contents": [],
+                "adjustMode": "shrink-to-fit"
+              },
+              {
+                "type": "text",
+                "text": "3學分",
+                "color": "#BFBFBFFF",
+                "align": "start",
+                "contents": [],
+                "adjustMode": "shrink-to-fit"
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    "background": {
+      "type": "linearGradient",
+      "angle": "45deg",
+      "startColor": "#020202C9",
+      "endColor": "#959595C9"
+    }
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": "授課老師",
+            "contents": []
+          },
+          {
+            "type": "text",
+            "text": "蘇XXXX",
+            "contents": []
+          }
+        ],
+        "spacing": "none",
+        "cornerRadius": "none",
+        "alignItems": "center"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": "開課系級",
+            "contents": []
+          },
+          {
+            "type": "text",
+            "text": "會計一甲",
+            "contents": []
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": "上課時間1",
+            "contents": []
+          },
+          {
+            "type": "text",
+            "text": "1-12",
+            "contents": []
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": "上課地點1",
+            "contents": []
+          },
+          {
+            "type": "text",
+            "text": "教學412",
+            "contents": []
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "加入願望清單",
+                  "uri": "https://linecorp.com"
+                },
+                "color": "#FF6868FF",
+                "style": "primary",
+                "adjustMode": "shrink-to-fit",
+                "gravity": "bottom"
+              },
+              {
+                "type": "separator",
+                "margin": "md"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "加入修課清單",
+                  "uri": "https://linecorp.com"
+                },
+                "style": "primary",
+                "adjustMode": "shrink-to-fit"
+              }
+            ]
+          },
+          {
+            "type": "separator",
+            "margin": "md"
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "上課用書",
+                  "uri": "https://linecorp.com"
+                },
+                "style": "primary",
+                "adjustMode": "shrink-to-fit",
+                "color": "#7F3D3DFF"
+              },
+              {
+                "type": "separator",
+                "margin": "md"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "課程大綱",
+                  "uri": "https://linecorp.com"
+                },
+                "style": "primary",
+                "adjustMode": "shrink-to-fit",
+                "color": "#000000"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
